ab46dfbc6267ac11cbc7f5b1185a32be11c434ad
Matias Lindgren <matias.lindgren@gmail.com>
2017-05-21 11:42:07 +0800

    add warnings to stdout when using unimplemented callbacks

--- src/WorldCallbacks.hpp
+++ src/WorldCallbacks.hpp
@@ -2,6 +2,7 @@
 #define WORLDCALLBACKS_HPP
 #include <Box2D/Box2D.h>
 
+#include <iostream>
 // Destruction listener for deleting the Polygon object assigned
 // to every userData pointer in bodies
 class DestructionListener : public b2DestructionListener
@@ -16,12 +17,15 @@ class DestructionListener : public b2DestructionListener
     void SayGoodbye(b2Fixture*);
     void SayGoodbye(b2Joint*)
     {
+      std::cout << "NOT IMPLEMENTED: DestructionListener::SayGoodbye with parameters (b2Joint*)" << std::endl;
     }
     void SayGoodbye(b2ParticleGroup*)
     {
+      std::cout << "NOT IMPLEMENTED: DestructionListener::SayGoodbye with parameters (b2ParticleGroup*)" << std::endl;
     }
     void SayGoodbye(b2ParticleSystem*, int32)
     {
+      std::cout << "NOT IMPLEMENTED: DestructionListener::SayGoodbye with parameters (b2ParticleSystem*, int32)" << std::endl;
     }
 };
 
