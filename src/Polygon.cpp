37999567ce7bbdd12364931e87d083cfd43ee6a0
Matias Lindgren <matias.lindgren@gmail.com>
2017-05-24 00:29:19 +0800

    demote Polygon from being sfml transformable

--- src/Polygon.cpp
+++ src/Polygon.cpp
@@ -1,12 +1,10 @@
 #include "Polygon.hpp"
 #include "Adapter.hpp"
 
-
-
 Polygon::Polygon(const b2Vec2 vertices[],
                  const unsigned int& vertex_count,
                  const sf::Color& color)
-  : vertices_(sf::VertexArray(sf::TrianglesFan, vertex_count))
+  : vertices_(sf::TrianglesFan, vertex_count)
 {
   for (auto i = 0; i < vertex_count; i++)
   {
@@ -15,15 +13,14 @@ Polygon::Polygon(const b2Vec2 vertices[],
   }
 }
 
-void Polygon::ApplyPhysics(const b2Transform& b2_transform)
-{
-  sf::Transformable::setPosition(ConvertVector(b2_transform.p));
-  sf::Transformable::setRotation(RadiansToAngle(b2_transform.q.GetAngle()));
-}
+/* void Polygon::ApplyPhysics(const b2Transform& b2_transform) */
+/* { */
+/*   sf::Transformable::setPosition(ConvertVector(b2_transform.p)); */
+/*   sf::Transformable::setRotation(RadiansToAngle(b2_transform.q.GetAngle())); */
+/* } */
+
 
 void Polygon::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-  states.transform *= getTransform();
   target.draw(vertices_, states);
 }
-
--- src/Polygon.hpp
+++ src/Polygon.hpp
@@ -3,19 +3,17 @@
 #include <SFML/Graphics.hpp>
 #include <Box2D/Box2D.h>
 
-
-class Polygon : public sf::Drawable, public sf::Transformable
+class Polygon : public sf::Drawable
 {
   public:
     Polygon(const b2Vec2 vertices[],
             const unsigned int& vertex_count,
             const sf::Color& color);
-    void ApplyPhysics(const b2Transform& b2_transform);
+    /* void ApplyPhysics(const b2Transform& b2_transform); */
 
   private:
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
     sf::VertexArray vertices_;
 };
-
 
 #endif // POLYGON.HPP
